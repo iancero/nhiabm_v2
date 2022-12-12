85351496f79c648fd4a8526901456cddfa4d7358
Ian Cero (URMC Laptop) <ian_cero@urmc.rochester.edu>
2022-12-11 20:47:13 -0500

    wrap param compilation in seperate function

--- simulation/parameter_combos.py
+++ simulation/parameter_combos.py
@@ -24,20 +24,28 @@ def parameter_combinations(param_dict, runs_per_combo, nonvarying_names=None):
     return param_combos
 
 
+def compile_all_parameter_cominations(param_dict, runs_per_combo):
+    # Get combos of intervention parameters first, then put combos back in params object
+    intervention_params = param_dict.pop("intervention_params")
+    intervention_combos = parameter_combinations(intervention_params, runs_per_combo=1)
+    param_dict.update({"intervention_params": intervention_combos})
+
+    # Create all combinations of parameters and interventions
+    param_combos = parameter_combinations(
+        param_dict=param_dict,
+        runs_per_combo=runs_per_combo,
+        nonvarying_names=["baserates", "sui_ORs"],
+    )
+
+    return param_combos
+
+
 if __name__ == "__main__":
 
     with open("input_params.json", "r") as f:
         params = json.load(f)
 
-    # Get combos of intervention parameters first, then put combos back in params object
-    intervention_params = params.pop("intervention_params")
-    intervention_combos = parameter_combinations(intervention_params, runs_per_combo=1)
-    params.update({"intervention_params": intervention_combos})
-
-    # Create all combinations of parameters and interventions
-    param_combos = parameter_combinations(
-        param_dict=params, runs_per_combo=1, nonvarying_names=["baserates", "sui_ORs"]
-    )
+    param_combos = compile_all_parameter_cominations(params, runs_per_combo=10)
 
     with open("experiments/input_parameter_combinations.json", "w") as f:
         f.write(json.dumps(param_combos))
